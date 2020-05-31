0014cbed7aee863798d940a7fdaa996ee78e226e
GhazASC5 <ghazanfarshahbaz2409@gmail.com>
2020-05-31 19:57:25 -0400

    add button

--- bookReccomendation.js
+++ bookReccomendation.js
@@ -1,5 +1,6 @@
 function bookSearch(){
     var search = document.getElementById('search').value
+    var a ='<a href="#" data-theme="b" data-role="button">',b='</a>'
     document.getElementById('results').innerHTML = ""
     console.log(search)
 
@@ -10,14 +11,20 @@ function bookSearch(){
         success: function(data){
             console.log(data)
             for(i = 0 ; i < data.items.length ; i++){
-                results.innerHTML += "<div class = container"
-                results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
-                results.innerHTML += "<img src = " + data.items[i].volumeInfo.imageLinks.thumbnail + ">"
-                results.innerHTML += "</div>"
-                // results.innerHTML += "<br></br>"
+                results.innerHTML += "<div class = card id = card"  + i + ">"  + "<h4 id = title style = text-align:center;>" + data.items[i].volumeInfo.title + "</h4>" + "<img src = " + data.items[i].volumeInfo.imageLinks.thumbnail + "id = bookPicture" + i + " style = width:100%;>" + "<div class = container>" +"<p><button class = w3-button-xlarge type = button onclick = addBookToYourList(this.id) style = width:100%; id =" + i + " >Add</button></p>" + "</div>" + "</div>"
             }
         },
         type: "GET"
     });
 }
+function addBookToYourList(id){
+    document.getElementById(id).remove()
+    var cardDiv =  document.getElementById("card" +id);
+    var clone = cardDiv.cloneNode(true);
+    var list= document.getElementById("yourList")
+    list.appendChild(clone);
+    cardDiv.remove();
+    
+}
+
 document.getElementById('button').addEventListener('click', bookSearch, false)
